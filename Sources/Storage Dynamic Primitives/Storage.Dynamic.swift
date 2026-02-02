8a7df5e175563e48ecda3a2f3be226b70c1e09ce
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-02-02 11:52:59 +0100

    WIP

---
+++ Sources/Storage Dynamic Primitives/Storage.Dynamic.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  swift-storage-primitives
+//
+//  Created by Coen ten Thije Boonkkamp on 02/02/2026.
+//
+
+import Storage_Primitives_Core
+
+extension Storage {
+    public typealias Dynamic = Storage_Primitives_Core.Storage
+}
